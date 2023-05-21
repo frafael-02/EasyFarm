10843db4f4f18bcfc285a1b32ade1a5e77e3fbcc
AntonioMiser0 <antoniomiser12@gmail.com>
2023-05-21 21:08:18 +0200

    uređen je prvi activity, dodani su login i register activitji uređen je i login samo treba sve dobro još spojiti složeni je register uređen je fragment_evidencija.xml z filterima treba dodati adapptera za SearchActivity

---
+++ app/src/main/java/com/example/testapp/SearchActivty.java
@@ -0,0 +1,74 @@
+package com.example.testapp;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.testapp.shop.ChildAdapter;
+import com.example.testapp.shop.ShopChildModelClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SearchActivty extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<ShopChildModelClass> itemList;
+    private ChildAdapter itemAdapter;
+    private SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        searchView=findViewById(R.id.searchView);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return false;
+            }
+        });
+
+        recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        itemList=new ArrayList<>();
+
+        itemList.add(new ShopChildModelClass(R.drawable.pesticid,"pesticid"));
+        itemList.add(new ShopChildModelClass(R.drawable.backwheat,"pesticid"));
+        itemList.add(new ShopChildModelClass(R.drawable.backlogin,"pesticid"));
+        itemList.add(new ShopChildModelClass(R.drawable.backwheat,"pesticid"));
+        itemList.add(new ShopChildModelClass(R.drawable.backlogin,"pesticid"));
+
+        itemAdapter=new ChildAdapter(itemList,this);
+        recyclerView.setAdapter(itemAdapter);
+    }
+
+    private void filterList(String text) {
+        List<ShopChildModelClass> filteredList = new ArrayList<>();
+        for(ShopChildModelClass item:itemList){
+            if(item.getItemName().toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(item);
+            }
+        }
+        if(filteredList.isEmpty()){
+            Toast.makeText(this, "Nema rezultata", Toast.LENGTH_SHORT).show();
+        }
+        else{
+itemAdapter.setFilteredList(filteredList);
+        }
+    }
+}
