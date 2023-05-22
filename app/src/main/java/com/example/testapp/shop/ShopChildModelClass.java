b02ae58b2e3f2726c3a6a0b3060f65c805474de0
frafael-02 <frafael@tvz.hr>
2023-05-22 16:26:15 +0200

    Add files via upload

--- app/src/main/java/com/example/testapp/shop/ShopChildModelClass.java
+++ app/src/main/java/com/example/testapp/shop/ShopChildModelClass.java
@@ -1,16 +1,25 @@
 package com.example.testapp.shop;
 
+import com.example.testapp.entiteti.Pesticid;
+
 public class ShopChildModelClass {
 
     int image;
     String text;
 
-    public ShopChildModelClass(int image, String text) {
+    Pesticid pesticid;
+
+    public ShopChildModelClass(int image, Pesticid pesticid) {
         this.image = image;
-        this.text = text;
+        this.pesticid = pesticid;
+        text = pesticid.getNaziv();
     }
 
     public String getItemName() {
     return text;
+    }
+
+    public Pesticid getPesticid() {
+        return pesticid;
     }
 }
